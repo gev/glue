195bde54488f608fd9f02ff5f5f82a7015c6d116
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-11-30 20:45:23 +0300

    initialize

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for reacthome-auth
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import           App
+import           Network.Wai.Handler.Warp
+
+main :: IO ()
+main =  run 8080 app
