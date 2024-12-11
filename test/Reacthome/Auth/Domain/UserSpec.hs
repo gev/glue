57844092e06772f157114728721b70072d52d788
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-12-12 02:56:05 +0300

    more tessts

--- test/Reacthome/Auth/Domain/UserSpec.hs
+++ test/Reacthome/Auth/Domain/UserSpec.hs
@@ -1,12 +1,12 @@
 module Reacthome.Auth.Domain.UserSpec (spec) where
 
 import Data.Maybe (fromJust)
-import Reacthome.Auth.Domain.User (UserId, UserLogin, UserPassword, UserStatus (..), createdAt, login, mkActiveUser, mkUser, passwordHash, status, uid, updatedAt)
+import Reacthome.Auth.Domain.User
 import Reacthome.Auth.Domain.UserId (mkUserId)
 import Reacthome.Auth.Domain.UserLogin (isValidUserLogin, mkUserLogin)
 import Reacthome.Auth.Domain.UserPassword (isValidUserPassword, mkUserPassword)
 import Test.Hspec (Spec, describe, it, shouldBe, shouldNotBe)
-import Test.QuickCheck (arbitrary, forAll, (==>))
+import Test.QuickCheck (arbitrary, elements, forAll, (==>))
 import Test.QuickCheck.Arbitrary (Arbitrary)
 import Test.QuickCheck.Gen (Gen, suchThat)
 import Test.QuickCheck.Instances.Text ()
@@ -55,6 +55,57 @@ spec =
                             ==> mkUser uid1 login1 password1 time1
                         `shouldNotBe` mkUser uid2 login2 password2 time2
 
+        it "Change an User Login successfully"
+            . property
+            $ forAll arbitrary \(uid, login, password, time) ->
+                forAll arbitrary \(newLogin, updatedAt) ->
+                    let user = mkUser uid login password time
+                        user' = changeUserLogin user newLogin updatedAt
+                     in (user.uid `shouldBe` user'.uid)
+                            <> (user'.login `shouldBe` newLogin)
+                            <> (user'.passwordHash `shouldBe` user.passwordHash)
+                            <> (user'.status `shouldBe` user.status)
+                            <> (user'.createdAt `shouldBe` user.createdAt)
+                            <> (user'.updatedAt `shouldBe` updatedAt)
+
+        it "Change an User Password successfully"
+            . property
+            $ forAll arbitrary \(uid, login, password, time) ->
+                forAll arbitrary \(newPasswordHash, updatedAt) ->
+                    let user = mkUser uid login password time
+                        user' = changeUserPassword user newPasswordHash updatedAt
+                     in (user.uid `shouldBe` user'.uid)
+                            <> (user'.login `shouldBe` user.login)
+                            <> (user'.passwordHash `shouldBe` newPasswordHash)
+                            <> (user'.status `shouldBe` user.status)
+                            <> (user'.createdAt `shouldBe` user.createdAt)
+                            <> (user'.updatedAt `shouldBe` updatedAt)
+
+        it "Change an User Status successfully"
+            . property
+            $ forAll arbitrary \(uid, login, password, time) ->
+                forAll arbitrary \(newStatus, updatedAt) ->
+                    let user = mkUser uid login password time
+                        user' = changeUserStatus user newStatus updatedAt
+                     in (user.uid `shouldBe` user'.uid)
+                            <> (user'.login `shouldBe` user.login)
+                            <> (user'.passwordHash `shouldBe` user.passwordHash)
+                            <> (user'.status `shouldBe` newStatus)
+                            <> (user'.createdAt `shouldBe` user.createdAt)
+                            <> (user'.updatedAt `shouldBe` updatedAt)
+
+        it "User is active when status is Active"
+            . property
+            $ forAll arbitrary \(uid, login, password, time) ->
+                let user = mkActiveUser uid login password time
+                 in isUserActive user `shouldBe` user.status == Active
+
+        it "User is inactive when status is Inactive"
+            . property
+            $ forAll arbitrary \(uid, login, password, time) ->
+                let user = mkUser uid login password time
+                 in isUserInactive user `shouldBe` user.status == Inactive
+
 instance Arbitrary UserId where
     arbitrary = mkUserId <$> arbitrary
 
@@ -63,6 +114,9 @@ instance Arbitrary UserLogin where
 
 instance Arbitrary UserPassword where
     arbitrary = mkArbitrary mkUserPassword isValidUserPassword
+
+instance Arbitrary UserStatus where
+    arbitrary = elements [Active, Inactive]
 
 mkArbitrary :: (Arbitrary a) => (a -> Maybe m) -> (a -> Bool) -> Gen m
 mkArbitrary mk isValid = fromJust . mk <$> arbitrary `suchThat` isValid
