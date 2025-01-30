5f2134efe2277891dbf7093a2cd1e7a3d9acd938
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-01-31 01:15:36 +0300

    store registered user

--- src/Reacthome/Auth/Controller/Register/Finish.hs
+++ src/Reacthome/Auth/Controller/Register/Finish.hs
@@ -6,11 +6,13 @@ import Reacthome.Auth.Controller.WebAuthn.PublicKeyCredential
 import Reacthome.Auth.Controller.WebAuthn.PublicKeyCredentialRpEntity
 import Reacthome.Auth.Controller.WebAuthn.PublicKeyCredentialUserEntity
 import Reacthome.Auth.Controller.WebAuthn.RegisteredOptions
+import Reacthome.Auth.Domain.Users
 import Reacthome.Auth.Environment
 
 finishRegister ::
     ( ?environment :: Environment
     , ?challenges :: RegisterChallenges
+    , ?users :: Users
     ) =>
     EncodedPublicKeyCredential ->
     IO (Either String RegisteredOptions)
@@ -19,6 +21,7 @@ finishRegister = mkRegisteredOptions . decodePublicKeyCredential
 mkRegisteredOptions ::
     ( ?environment :: Environment
     , ?challenges :: RegisterChallenges
+    , ?users :: Users
     ) =>
     Either String DecodedPublicKeyCredential ->
     IO (Either String RegisteredOptions)
@@ -27,10 +30,15 @@ mkRegisteredOptions (Right credentials) = do
     let challenge = mkChallenge credentials.challenge
     user' <- ?challenges.get challenge
     ?challenges.remove challenge
-    pure do
-        user <- user'
-        Right
-            RegisteredOptions
-                { rp = mkPublicKeyCredentialRpEntity
-                , user = mkPublicKeyCredentialUserEntity user
-                }
+    case user' of
+        Left err -> pure $ Left err
+        Right user -> do
+            success <- ?users.store user
+            pure case success of
+                Left err -> Left err
+                _ ->
+                    Right
+                        RegisteredOptions
+                            { rp = mkPublicKeyCredentialRpEntity
+                            , user = mkPublicKeyCredentialUserEntity user
+                            }
