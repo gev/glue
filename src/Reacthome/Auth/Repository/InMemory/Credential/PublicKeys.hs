dc2139961621bc4f924418d70ea6c3f337bfb824
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-01-31 17:32:13 +0300

    the in memory implementation of the public keys repository

--- src/Reacthome/Auth/Repository/InMemory/Credential/PublicKeys.hs
+++ src/Reacthome/Auth/Repository/InMemory/Credential/PublicKeys.hs
@@ -1,1 +1,42 @@
 module Reacthome.Auth.Repository.InMemory.Credential.PublicKeys where
+
+import Control.Concurrent.MVar
+import Data.HashMap.Strict
+import Reacthome.Auth.Domain.Credential.PublicKey
+import Reacthome.Auth.Domain.Credential.PublicKeys
+import Util.MVar
+import Prelude hiding (lookup)
+
+mkPublicKeys :: IO PublicKeys
+mkPublicKeys = do
+    map' <- newMVar empty
+    let
+        get id' =
+            runRead
+                map'
+                \publicKeys ->
+                    case lookup id' publicKeys of
+                        (Just publicKey) -> Right publicKey
+                        _ -> Left $ "Public key not found " <> show id'
+
+        store publicKey =
+            modifyMVar
+                map'
+                $ \publicKeys ->
+                    pure case lookup publicKey.id publicKeys of
+                        (Just _) ->
+                            ( publicKeys
+                            , Left "Public key already exists"
+                            )
+                        _ ->
+                            ( insert publicKey.id publicKey publicKeys
+                            , Right ()
+                            )
+
+        remove = runModify map' . delete
+    pure
+        PublicKeys
+            { get
+            , store
+            , remove
+            }
