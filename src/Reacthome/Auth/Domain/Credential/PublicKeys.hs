b0c95c79cf3242c7881c04c3a4eb69d33f1b466a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-01-31 17:19:22 +0300

    interface of the public keys repository

--- src/Reacthome/Auth/Domain/Credential/PublicKeys.hs
+++ src/Reacthome/Auth/Domain/Credential/PublicKeys.hs
@@ -1,1 +1,10 @@
 module Reacthome.Auth.Domain.Credential.PublicKeys where
+
+import Reacthome.Auth.Domain.Credential.PublicKey
+import Reacthome.Auth.Domain.Credential.PublicKey.Id
+
+data PublicKeys = PublicKeys
+    { get :: PublicKeyId -> IO (Either String PublicKey)
+    , store :: PublicKey -> IO (Either String ())
+    , remove :: PublicKeyId -> IO ()
+    }
