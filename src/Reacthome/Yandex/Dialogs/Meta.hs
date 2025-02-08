664f35a23a181d202c20d794476b9471cf0ecb32
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-02-08 22:21:49 +0300

    cleanup

--- src/Reacthome/Yandex/Dialogs/Meta.hs
+++ src/Reacthome/Yandex/Dialogs/Meta.hs
@@ -3,13 +3,11 @@ module Reacthome.Yandex.Dialogs.Meta where
 import Data.Aeson
 import Data.Text
 import GHC.Generics
-import Reacthome.Yandex.Dialogs.Interfaces
 
 data Meta = Meta
     { locale :: Text
     , timezone :: Text
     , client_id :: Text
-    , interfaces :: Interfaces
     }
     deriving stock (Generic, Show)
     deriving anyclass (FromJSON)
