fe7afda47a855e20a05584b578cb4cf510fac02b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-02-12 04:10:20 +0300

    go back to the static content

--- src/Reacthome/Auth/App.hs
+++ src/Reacthome/Auth/App.hs
@@ -13,8 +13,6 @@ import Reacthome.Auth.Domain.Credential.PublicKeys
 import Reacthome.Auth.Domain.Users
 import Reacthome.Auth.Environment
 import Reacthome.Auth.Service.Challenges (Challenges)
-import Reacthome.Auth.View.Screen.Authentication
-import Reacthome.Auth.View.Screen.Registration
 import Util.Wai
 
 app ::
@@ -26,18 +24,14 @@ app ::
     Application
 app req respond
     | req.requestMethod == methodGet
-        || req.requestMethod == methodHead = do
-        let respond' = respond . makeHTML
-        case req.pathInfo of
-            [] -> respond' authentication
-            ["register"] -> respond' registration
-            _ -> static req respond
+        || req.requestMethod == methodHead =
+        static req respond
     | req.requestMethod == methodPost = do
         let respond' ::
                 (FromJSON req, ToJSON res) =>
                 (req -> ExceptT String IO res) ->
                 IO ResponseReceived
-            respond' = makeJSON req respond
+            respond' = makeRespond req respond
         case req.pathInfo of
             ["registration", "begin"] -> respond' beginRegistration
             ["registration", "complete"] -> respond' completeRegistration
