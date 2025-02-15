8eb9cf09bf0ee16fbb514338997716aff3e58611
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-02-16 01:47:33 +0300

    Yandex dialog should require authorization

--- src/Reacthome/Assist/Controller/Yandex.hs
+++ src/Reacthome/Assist/Controller/Yandex.hs
@@ -4,25 +4,29 @@ import Control.Monad.Trans.Class
 import Control.Monad.Trans.Except
 import Reacthome.Yandex.Dialogs.DialogRequest
 import Reacthome.Yandex.Dialogs.DialogResponse
-import Reacthome.Yandex.Dialogs.Request
-import Reacthome.Yandex.Dialogs.Response
-
--- import Reacthome.Yandex.Dialogs.Request
+import Reacthome.Yandex.Dialogs.Directives
+import Reacthome.Yandex.Dialogs.Request qualified as D
+import Reacthome.Yandex.Dialogs.Response qualified as D
+import Web.Rest
+import Web.Rest.Media
+import Web.Rest.Status
 
 runDialog ::
-    DialogRequest ->
-    ExceptT String IO DialogResponse
-runDialog dialog = do
-    let greeting = dialog.request.command
-    lift $ print dialog
-    pure
-        DialogResponse
-            { response =
-                Response
-                    { text = greeting
-                    , tts = Just greeting
-                    , end_session = False
-                    , directives = Nothing -- Just start'account'linking
-                    }
-            , version = "1.0"
-            }
+    (?request :: Request) =>
+    IO Response
+runDialog =
+    either badRequest toJSON =<< runExceptT do
+        dialog <- fromJSON @DialogRequest ?request
+        let greeting = dialog.request.command
+        -- lift $ print dialog
+        pure
+            DialogResponse
+                { response =
+                    D.Response
+                        { text = greeting
+                        , tts = Just greeting
+                        , end_session = False
+                        , directives = Just start'account'linking
+                        }
+                , version = "1.0"
+                }
