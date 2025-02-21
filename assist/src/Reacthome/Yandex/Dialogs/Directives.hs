1d18fcf3d7396fca1ff067e37edfd87e7bef492f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-02-22 02:20:40 +0300

    clean

--- assist/src/Reacthome/Yandex/Dialogs/Directives.hs
+++ assist/src/Reacthome/Yandex/Dialogs/Directives.hs
@@ -2,15 +2,12 @@ module Reacthome.Yandex.Dialogs.Directives where
 
 import Data.Aeson
 import GHC.Generics
-import Util.Aeson
 
 newtype Directives = Directives
     { start_account_linking :: Maybe Value
     }
     deriving stock (Generic, Show)
-
-instance ToJSON Directives where
-    toJSON = genericToJSON typeFieldLabelModifier
+    deriving anyclass (ToJSON)
 
 start'account'linking :: Directives
 start'account'linking =
