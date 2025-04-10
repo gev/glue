41b64bd6220e37beccc4bca6058c5b19df9f6b09
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-04-10 10:36:36 +0300

    spaces

--- auth/src/Reacthome/Auth/Domain/User/Id.hs
+++ auth/src/Reacthome/Auth/Domain/User/Id.hs
@@ -5,8 +5,8 @@ import Data.UUID
 import Data.UUID.V4
 
 newtype UserId = UserId {value :: UUID}
-  deriving stock (Show)
-  deriving newtype (Eq, Hashable)
+    deriving stock (Show)
+    deriving newtype (Eq, Hashable)
 
 makeUserId :: UUID -> UserId
 makeUserId = UserId
--- auth/src/Reacthome/Auth/Domain/User/Name.hs
+++ auth/src/Reacthome/Auth/Domain/User/Name.hs
@@ -4,19 +4,19 @@ import Control.Monad.Trans.Except
 import Data.Text as Text
 
 newtype UserName = UserName {value :: Text}
-  deriving stock (Show)
-  deriving newtype (Eq)
+    deriving stock (Show)
+    deriving newtype (Eq)
 
 makeUserName :: (Monad m) => Text -> ExceptT String m UserName
 makeUserName name =
-  if isValidUserName name'
-    then pure $ UserName name'
-    else throwE "Name should be between 3 and 64 characters"
- where
-  name' = Text.strip name
+    if isValidUserName name'
+        then pure $ UserName name'
+        else throwE "Name should be between 3 and 64 characters"
+  where
+    name' = Text.strip name
 
 isValidUserName :: Text -> Bool
 isValidUserName name =
-  length' >= 3 && length' <= 64
- where
-  length' = Text.length name
+    length' >= 3 && length' <= 64
+  where
+    length' = Text.length name
