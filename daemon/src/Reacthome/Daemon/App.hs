4a908ae36d4c0b7d10e3492982c87b22897ffcf3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-11-18 21:57:23 +0300

    up

--- daemon/src/Reacthome/Daemon/App.hs
+++ daemon/src/Reacthome/Daemon/App.hs
@@ -1,6 +1,5 @@
 module Reacthome.Daemon.App where
 
-import Control.Concurrent (threadDelay)
 import Data.Foldable (traverse_)
 import Data.Text.Lazy (show)
 import Data.Text.Lazy.Encoding
@@ -12,15 +11,13 @@ import Prelude hiding (show)
 
 application :: UUID -> WebSocketClientApplication
 application peer connection = do
-    client.start
-    traverse_ loop [0 ..]
-  where
-    client = makeRelayClient peer connection
-    loop count = do
-        client.send
-            RelayMessage
-                { peer
-                , content = encodeUtf8 $ "Hello Relay! " <> show @Int count
-                }
-
--- threadDelay 1000
+  client.start
+  traverse_ loop [0 ..]
+ where
+  client = makeRelayClient peer connection
+  loop count = do
+    client.send
+      RelayMessage
+        { peer
+        , content = encodeUtf8 $ "Hello Relay! " <> show @Int count
+        }
