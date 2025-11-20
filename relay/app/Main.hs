179139363cc75a3dba2bdb6041bf06fc39d04194
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-11-20 22:49:52 +0300

    rollback

--- relay/app/Main.hs
+++ relay/app/Main.hs
@@ -1,9 +1,11 @@
 import Reacthome.Relay.App (application)
+import Reacthome.Relay.Server (makeRelayServer)
 import Web.WebSockets.Server (runWebSocketServer)
 
 main :: IO ()
 main = do
     let port = 3003
         host = "0.0.0.0"
+    server <- makeRelayServer
     putStrLn $ "Run Reacthome Relay on " <> host <> ":" <> show port
-    runWebSocketServer host port application
+    runWebSocketServer host port $ application server
