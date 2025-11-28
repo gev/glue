c4543cb4bedc6cfeb8e184c7455d68f618bc222b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-11-29 02:00:46 +0300

    run timer forever!

--- util/src/Util/Timer.hs
+++ util/src/Util/Timer.hs
@@ -1,7 +1,7 @@
 module Util.Timer where
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (void)
+import Control.Monad (forever, void)
 import Data.IORef (modifyIORef', newIORef, readIORef)
 
 newtype Timer = Timer {ticks :: IO Int}
@@ -10,7 +10,7 @@ makeTimer :: Int -> IO Timer
 makeTimer usInterval = do
     timer <- newIORef 0
 
-    void $ forkIO do
+    void . forkIO $ forever do
         modifyIORef' timer (+ 1)
 
     threadDelay usInterval
