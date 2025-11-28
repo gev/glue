c145c2827e9c702706f7fb1fe5f41062edeb67db
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-11-28 14:09:38 +0300

    revert the relay

--- relay/src/Reacthome/Relay/Server.hs
+++ relay/src/Reacthome/Relay/Server.hs
@@ -1,6 +1,6 @@
 module Reacthome.Relay.Server where
 
-import Control.Concurrent (threadDelay, yield)
+import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (race_)
 import Control.Exception (finally, handle)
 import Control.Monad (forever, unless)
@@ -33,34 +33,32 @@ makeRelayServer dispatcher = do
             let
                 runRx connection = wrap $ forever do
                     !message <- connection.receiveMessage
-                    yield
-                    connection.sendMessage message
-                -- let destination = getMessageDestination message
-                -- if isMessageDestinationValid destination
-                --     then do
-                --         !found <- dispatcher.getSink destination
-                --         case found of
-                --             Just !sink -> sink.sendMessage message
-                --             Nothing -> logError $ NoPeersFound destination
-                --     else logError $ InvalidDestination destination
+                    let destination = getMessageDestination message
+                    if isMessageDestinationValid destination
+                        then do
+                            !found <- dispatcher.getSink destination
+                            case found of
+                                Just !sink -> sink.sendMessage message
+                                Nothing -> logError $ NoPeersFound destination
+                        else logError $ InvalidDestination destination
 
-                -- runTx connection source = do
-                --     buffer <- newIORef empty
-                --     let
-                --         collectMessages = forever do
-                --             !message <- source.receiveMessage
-                --             atomicModifyIORef'_ buffer (|> message)
+                runTx connection source = do
+                    buffer <- newIORef empty
+                    let
+                        collectMessages = forever do
+                            !message <- source.receiveMessage
+                            atomicModifyIORef'_ buffer (|> message)
 
-                --         transmitMessages = wrap $ forever do
-                --             !messages <- atomicModifyIORef' buffer (empty,)
-                --             unless (null messages) do
-                --                 let !chunks = chunksOf batchSize $ toList messages
-                --                 traverse_ connection.sendMessages chunks
-                --             threadDelay flushIntervalUs
+                        transmitMessages = wrap $ forever do
+                            !messages <- atomicModifyIORef' buffer (empty,)
+                            unless (null messages) do
+                                let !chunks = chunksOf batchSize $ toList messages
+                                traverse_ connection.sendMessages chunks
+                            threadDelay flushIntervalUs
 
-                --     race_
-                --         collectMessages
-                --         transmitMessages
+                    race_
+                        collectMessages
+                        transmitMessages
 
                 from = toStrict $ toByteString peer
 
@@ -69,17 +67,16 @@ makeRelayServer dispatcher = do
 
             wrap do
                 connection <- pending.accept
-                runRx connection
-    -- print $ "Peer connected " <> show peer
-    -- source <- dispatcher.getSource from
-    -- finally
-    --     do
-    --         race_
-    --             do runRx connection
-    --             do runTx connection source
-    --     do
-    --         -- print $ "Peer disconnected " <> show peer
-    --         dispatcher.freeSource from
+                -- print $ "Peer connected " <> show peer
+                source <- dispatcher.getSource from
+                finally
+                    do
+                        race_
+                            do runRx connection
+                            do runTx connection source
+                    do
+                        -- print $ "Peer disconnected " <> show peer
+                        dispatcher.freeSource from
 
     RelayServer{..}
 
