95db75974fc1cef8fabbb68663856a72d1498ce9
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-12-04 00:54:56 +0300

    use defaultOPtions

--- relay/app/Main.hs
+++ relay/app/Main.hs
@@ -1,6 +1,6 @@
 import Reacthome.Relay.App (application)
 import Reacthome.Relay.Dispatcher (makeRelayDispatcher)
-import Reacthome.Relay.Options (RelayOptions (..))
+import Reacthome.Relay.Options (defaultOptions)
 import Reacthome.Relay.Server (makeRelayServer)
 import Web.WebSockets.Server (runWebSocketServer)
 
@@ -9,11 +9,7 @@ main =
   run "0.0.0.0" 3003
  where
   run host port = do
-    let ?options =
-          RelayOptions
-            { inBound = 64
-            , chunkSize = 64
-            }
+    let ?options = defaultOptions
     dispatcher <- makeRelayDispatcher
     let ?dispatcher = dispatcher
     let server = makeRelayServer
