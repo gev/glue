ca4666a43ea683acb16b2b6231be72424955683a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-12-03 20:50:51 +0300

    up

--- daemon/app/Main.hs
+++ daemon/app/Main.hs
@@ -1,7 +1,5 @@
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (mapConcurrently_, race_)
-import Control.Concurrent.Chan.Unagi (newChan, readChan)
-import Control.Concurrent.Chan.Unagi.Bounded qualified as B
 import Control.Exception (catch)
 import Control.Monad (forever, replicateM, void)
 import Data.ByteString (toStrict)
@@ -18,6 +16,7 @@ import Reacthome.Relay.Message (RelayMessage (..), serializeMessage)
 import Reacthome.Relay.Stat (RelayHits (..), RelayStat (..), RelayStatHits (..), makeRelayStat)
 import System.Clock (Clock (..), diffTimeSpec, getTime, toNanoSecs)
 import Web.WebSockets.Client (runWebSocketClient)
+import Web.WebSockets.Connection (WebSocketConnection (sendMessages))
 import Web.WebSockets.Error (WebSocketError)
 import Prelude hiding (last)
 
@@ -27,32 +26,27 @@ concurrency = 10_000
 messagesPerChunk :: Int
 messagesPerChunk = 1
 
-bound :: Int
-bound = 1
-
 main :: IO ()
 main = do
     peers <- replicateM concurrency nextRandom
-    (peerInChans, peerOutChans) <- unzip <$> replicateM concurrency (B.newChan bound)
-    (mainInChan, mainOutChan) <- newChan
+    connections <- newIORef []
     stat <- makeRelayStat
-    connections <- newIORef @Int 0
     let
         port = 3003
         host = "172.16.1.1"
 
-        run (peer, outChan, delay) = do
+        run (peer, delay) = do
             threadDelay delay
-            void $ atomicModifyIORef'_ connections (+ 1)
             catch @WebSocketError
                 do
-                    let ?inChan = mainInChan
-                    let ?outChan = outChan
                     let path = "/" <> show peer
-                    -- putStrLn $ "Connect to Reacthome Relay on " <> host <> ":" <> show port <> path
-                    runWebSocketClient host port path $ application peer
+                    let ?register =
+                            \connection ->
+                                void $ atomicModifyIORef'_ connections (connection :)
+                    let ?onMessage = const $ stat.rx.hit 1
+                    let ?onError = print
+                    runWebSocketClient host port path application
                 print
-            void $ atomicModifyIORef'_ connections \n -> n - 1
 
         rps x1 x0 dt = fmt x1 <> " " <> " | RPS: " <> fmt ((x1 - x0) `div` dt)
 
@@ -65,7 +59,7 @@ main = do
             t1 <- getTime Monotonic
             hits1 <- stat.hits
             let !dt = fromInteger $ toNanoSecs (diffTimeSpec t1 t0) `div` 1_000_000_000
-            n <- readIORef connections
+            n <- length <$> readIORef connections
             putStrLn $ "<-> " <> fmt n <> " connections"
             putStrLn $ "Tx: " <> rps hits1.tx hits0.tx dt
             putStrLn $ "Rx: " <> rps hits1.rx hits0.rx dt
@@ -83,19 +77,14 @@ main = do
         do
             race_
                 do
-                    mapConcurrently_ run $ zip3 peers peerOutChans [1_000, 2_000 ..]
+                    mapConcurrently_ run $ zip peers [1_000, 2_000 ..]
                 showStat
         do
-            race_
-                do
-                    threadDelay 20_000_000
-                    forever do
-                        for_
-                            (zip peerInChans messagesPool)
-                            \(inChan, messages) -> do
-                                B.writeChan inChan messages
-                                stat.tx.hit messagesPerChunk
-                do
-                    forever do
-                        void $ readChan mainOutChan
-                        stat.rx.hit 1
+            threadDelay 20_000_000
+            forever do
+                connections' <- readIORef connections
+                for_
+                    (zip connections' messagesPool)
+                    \(connection, messages) -> do
+                        connection.sendMessages messages
+                        stat.tx.hit messagesPerChunk
--- daemon/src/Reacthome/Daemon/App.hs
+++ daemon/src/Reacthome/Daemon/App.hs
@@ -4,9 +4,7 @@ module Reacthome.Daemon.App where
 
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (race_)
-import Control.Concurrent.Chan.Unagi (InChan, OutChan, readChan, writeChan)
-import Control.Concurrent.Chan.Unagi.Bounded qualified as B
-import Control.Exception (handle)
+import Control.Exception (catch, handle)
 import Control.Monad (forever, void)
 import Data.ByteString (toStrict)
 import Data.Text.Encoding
@@ -19,28 +17,16 @@ import Web.WebSockets.Connection (WebSocketConnection (..))
 import Web.WebSockets.Error (WebSocketError)
 
 application ::
-    ( ?inChan :: InChan StrictRaw
-    , ?outChan :: B.OutChan [StrictRaw]
+    ( ?register :: WebSocketConnection -> IO ()
+    , ?onMessage :: StrictRaw -> IO ()
+    , ?onError :: WebSocketError -> IO ()
     ) =>
-    UUID -> WebSocketClientApplication
-application peer connection = do
-    let
-        from = toStrict $ toByteString peer
-
-        onError = logError . WebSocketError from
-        wrap = handle @WebSocketError onError
-
-        runTx = do
+    WebSocketClientApplication
+application connection = do
+    ?register connection
+    catch @WebSocketError
+        do
             forever do
-                !messages <- B.readChan ?outChan
-                connection.sendMessages messages
-
-        runRx = forever do
-            !message <- connection.receiveMessage
-            writeChan ?inChan message
-
-    race_
-        do wrap runTx
-        do wrap runRx
-
-    print $ "Peer " <> show peer <> " Disconnected"
+                !message <- connection.receiveMessage
+                ?onMessage message
+        ?onError
