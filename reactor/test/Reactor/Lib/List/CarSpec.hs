5bd0a02c3f77b96b73005293fdadf1dfacc7358a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-12-28 21:58:22 +0300

    Split ListSpec into CarSpec and CdrSpec

---
+++ reactor/test/Reactor/Lib/List/CarSpec.hs
@@ -0,0 +1,33 @@
+module Reactor.Lib.List.CarSpec (spec) where
+
+import Reactor.Env qualified as E
+import Reactor.Eval (runEval)
+import Reactor.IR (IR (..))
+import Reactor.Lib.List.Car (car)
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Reactor.Lib.List.Car (Test car function)" do
+    it "returns the first element of a list" do
+        let initialEnv = E.emptyEnv
+        let args = [List [Number 1, Number 2, Number 3]]
+        result <- runEval (car args) initialEnv
+        case result of
+            Left err -> expectationFailure $ "Car failed: " <> show err
+            Right (res, _, _) -> res `shouldBe` Number 1
+
+    it "fails on empty list" do
+        let initialEnv = E.emptyEnv
+        let args = [List []]
+        result <- runEval (car args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Car should fail on empty list"
+
+    it "fails on non-list" do
+        let initialEnv = E.emptyEnv
+        let args = [Number 42]
+        result <- runEval (car args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Car should fail on non-list"
