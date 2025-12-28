0e84220dcbd700876b134213676d0d61b81577cf
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-12-28 22:15:12 +0300

    Add nth function to Reactor.Lib.List module

---
+++ reactor/test/Reactor/Lib/List/NthSpec.hs
@@ -0,0 +1,65 @@
+module Reactor.Lib.List.NthSpec (spec) where
+
+import Reactor.Env qualified as E
+import Reactor.Eval (runEval)
+import Reactor.IR (IR (..))
+import Reactor.Lib.List.Nth (nth)
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Reactor.Lib.List.Nth (Test nth function)" do
+    it "returns element at index 0" do
+        let initialEnv = E.emptyEnv
+        let args = [Number 0, List [Number 1, Number 2, Number 3]]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left err -> expectationFailure $ "Nth failed: " <> show err
+            Right (res, _, _) -> res `shouldBe` Number 1
+
+    it "returns element at index 1" do
+        let initialEnv = E.emptyEnv
+        let args = [Number 1, List [Number 1, Number 2, Number 3]]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left err -> expectationFailure $ "Nth failed: " <> show err
+            Right (res, _, _) -> res `shouldBe` Number 2
+
+    it "returns element at last index" do
+        let initialEnv = E.emptyEnv
+        let args = [Number 2, List [Number 1, Number 2, Number 3]]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left err -> expectationFailure $ "Nth failed: " <> show err
+            Right (res, _, _) -> res `shouldBe` Number 3
+
+    it "fails on negative index" do
+        let initialEnv = E.emptyEnv
+        let args = [Number (-1), List [Number 1, Number 2, Number 3]]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Nth should fail on negative index"
+
+    it "fails on index out of bounds" do
+        let initialEnv = E.emptyEnv
+        let args = [Number 3, List [Number 1, Number 2, Number 3]]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Nth should fail on index out of bounds"
+
+    it "fails on non-number index" do
+        let initialEnv = E.emptyEnv
+        let args = [String "0", List [Number 1, Number 2, Number 3]]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Nth should fail on non-number index"
+
+    it "fails on non-list" do
+        let initialEnv = E.emptyEnv
+        let args = [Number 0, Number 42]
+        result <- runEval (nth args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Nth should fail on non-list"
