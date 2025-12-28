223636bd3ba921e40cd1900c98dee08b54ddaf0d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-12-28 23:19:20 +0300

    Add find and position functions to Reactor.Lib.List module

--- reactor/src/Reactor/Lib/List.hs
+++ reactor/src/Reactor/Lib/List.hs
@@ -10,11 +10,13 @@ import Reactor.Lib.List.Cdr (cdr)
 import Reactor.Lib.List.Cons (cons)
 import Reactor.Lib.List.Drop qualified as Drop
 import Reactor.Lib.List.Filter qualified as Filter
+import Reactor.Lib.List.Find qualified as Find
 import Reactor.Lib.List.Last qualified as Last
 import Reactor.Lib.List.Length qualified as Length
 import Reactor.Lib.List.Map qualified as Map
 import Reactor.Lib.List.Member (member)
 import Reactor.Lib.List.Nth (nth)
+import Reactor.Lib.List.Position qualified as Position
 import Reactor.Lib.List.Remove (remove)
 import Reactor.Lib.List.Reverse qualified as Reverse
 import Reactor.Lib.List.Take qualified as Take
@@ -29,11 +31,13 @@ list =
         , ("cons", Native (Func cons))
         , ("drop", Native (Func Drop.drop))
         , ("filter", Native (Func Filter.filter))
+        , ("find", Native (Func Find.find))
         , ("last", Native (Func Last.last))
         , ("length", Native (Func Length.length))
         , ("map", Native (Func Map.map))
         , ("member", Native (Func member))
         , ("nth", Native (Func nth))
+        , ("position", Native (Func Position.position))
         , ("remove", Native (Func remove))
         , ("reverse", Native (Func Reverse.reverse))
         , ("take", Native (Func Take.take))
---
+++ reactor/test/Reactor/Lib/List/FindSpec.hs
@@ -0,0 +1,54 @@
+module Reactor.Lib.List.FindSpec (spec) where
+
+import Reactor.Env qualified as E
+import Reactor.Eval (runEval)
+import Reactor.IR (IR (..), Native (..))
+import Reactor.Lib.List.Find qualified as Find
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Reactor.Lib.List.Find (Test find function)" do
+    it "finds first element that satisfies predicate" do
+        let initialEnv = E.emptyEnv
+        let pred = Native (Func (\[Number x] -> pure $ if x > 2 then Symbol "true" else Symbol "false"))
+        let args = [pred, List [Number 1, Number 2, Number 3, Number 4]]
+        result <- runEval (Find.find args) initialEnv
+        case result of
+            Left err -> expectationFailure $ "Find failed: " <> show err
+            Right (res, _, _) -> res `shouldBe` Number 3
+
+    it "finds first element in list" do
+        let initialEnv = E.emptyEnv
+        let pred = Native (Func (\[Number x] -> pure $ if x > 0 then Symbol "true" else Symbol "false"))
+        let args = [pred, List [Number 1, Number 2, Number 3]]
+        result <- runEval (Find.find args) initialEnv
+        case result of
+            Left err -> expectationFailure $ "Find failed: " <> show err
+            Right (res, _, _) -> res `shouldBe` Number 1
+
+    it "fails when no element satisfies predicate" do
+        let initialEnv = E.emptyEnv
+        let pred = Native (Func (\[Number x] -> pure $ if x > 10 then Symbol "true" else Symbol "false"))
+        let args = [pred, List [Number 1, Number 2, Number 3]]
+        result <- runEval (Find.find args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Find should fail when no element found"
+
+    it "fails on empty list" do
+        let initialEnv = E.emptyEnv
+        let pred = Native (Func (\[Number x] -> pure $ Symbol "true"))
+        let args = [pred, List []]
+        result <- runEval (Find.find args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Find should fail on empty list"
+
+    it "fails on non-list second argument" do
+        let initialEnv = E.emptyEnv
+        let pred = Native (Func (\[Number x] -> pure $ Symbol "true"))
+        let args = [pred, Number 42]
+        result <- runEval (Find.find args) initialEnv
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Find should fail on non-list"
