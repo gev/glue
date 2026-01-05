7d39e2b54a641b7593a7a694f10d7f7254cecca3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-05 17:57:11 +0300

    Add comprehensive tests for try special form

---
+++ haskell/glue/test/Glue/Lib/Builtin/TrySpec.hs
@@ -0,0 +1,54 @@
+module Glue.Lib.Builtin.TrySpec (spec) where
+
+import Data.Text (Text)
+import Glue.Env qualified as E
+import Glue.Error (GlueError (..))
+import Glue.Eval (Eval, eval, runEvalLegacy)
+import Glue.Eval.Error (EvalError (..), GeneralError (..))
+import Glue.IR (IR (..), compile)
+import Glue.Lib (lib)
+import Glue.Parser (parseGlue)
+import Test.Hspec
+
+runCode :: Text -> IO (Either GlueError (Maybe (IR Eval)))
+runCode input = case parseGlue input of
+    Left err -> pure $ Left (GlueError err)
+    Right ast -> do
+        let irTree = compile ast
+        fullResult <- runEvalLegacy (eval irTree) (E.fromFrame lib)
+        case fullResult of
+            Left err -> pure $ Left (GlueError err)
+            Right (res, _finalEnv, _ctx) -> pure $ Right res
+
+spec :: Spec
+spec = describe "Glue.Lib.Builtin.Try (Test try special form)" do
+    it "catches exception and calls handler with payload" do
+        let code = "(try (error test-error (:msg \"hello\")) (catch \"test-error\" (lambda (err) err.msg)))"
+        runCode code `shouldReturn` Right (Just (String "hello"))
+
+    it "returns normal value when no exception" do
+        let code = "(try 42 (catch \"any-error\" (lambda (err) \"caught\")))"
+        runCode code `shouldReturn` Right (Just (Number 42))
+
+    it "re-throws unmatched exception" do
+        let code = "(try (error test-error (:msg \"hello\")) (catch \"other-error\" (lambda (err) err.msg)))"
+        result <- runCode code
+        case result of
+            Left _ -> pure () -- Expected error
+            Right _ -> expectationFailure "Expected error for unmatched exception"
+
+    it "works with symbol catch names" do
+        let code = "(try (error test-error (:msg \"hello\")) (catch test-error (lambda (err) err.msg)))"
+        runCode code `shouldReturn` Right (Just (String "hello"))
+
+    it "handler can be any callable" do
+        let code = "(try (error test-error (:val 123)) (catch \"test-error\" (lambda (err) (+ err.val 1))))"
+        runCode code `shouldReturn` Right (Just (Number 124))
+
+    it "multiple catch clauses work" do
+        let code = "(try (error second-error (:msg \"second\")) (catch \"first-error\" (lambda (err) \"first\")) (catch \"second-error\" (lambda (err) err.msg)))"
+        runCode code `shouldReturn` Right (Just (String "second"))
+
+    it "first matching catch is used" do
+        let code = "(try (error test-error (:msg \"caught\")) (catch \"test-error\" (lambda (err) err.msg)) (catch \"test-error\" (lambda (err) \"second\")))"
+        runCode code `shouldReturn` Right (Just (String "caught"))
