bfdccbc75beaa794c42caddda274d0d6561ac71e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-14 21:10:17 +0300

    up

--- dart/glue/lib/src/error.dart
+++ dart/glue/lib/src/error.dart
@@ -3,13 +3,3 @@
 abstract interface class GlueError {
   String pretty();
 }
-
-/// Convenience function to create GlueError
-/// Mirrors Haskell's GlueError constructor
-GlueError glueError(dynamic error) {
-  if (error is GlueError) {
-    return error;
-  }
-  // For non-GlueError types, we could wrap them, but for now assume they're already GlueError
-  throw ArgumentError('Expected GlueError, got ${error.runtimeType}');
-}
--- dart/glue/test/eval_test.dart
+++ dart/glue/test/eval_test.dart
@@ -1,4 +1,3 @@
-import 'package:glue/src/ast.dart';
 import 'package:glue/src/either.dart';
 import 'package:glue/src/eval.dart';
 import 'package:glue/src/ir.dart';
@@ -12,9 +11,7 @@ import 'package:test/test.dart';
 /// Helper to run full Glue code like Haskell EvalSpec.hs
 Future<Either<GlueError, Ir>> runCode(String input) async {
   final parseResult = parseGlue(input);
-  return parseResult.match((parseError) => Left(glueError(parseError)), (
-    ast,
-  ) async {
+  return parseResult.match((parseError) => Left(parseError), (ast) async {
     final irTree = compile(ast);
     final env = envFromModules([
       builtin,
@@ -22,7 +19,7 @@ Future<Either<GlueError, Ir>> runCode(String input) async {
     final runtime = Runtime.initial(env);
 
     final evalResult = await runEval(eval(irTree), runtime);
-    return evalResult.match((error) => Left(glueError(error)), (value) {
+    return evalResult.match((error) => Left(error), (value) {
       final (result, _) = value;
       return Right(result);
     });
