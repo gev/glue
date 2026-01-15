344638445bbcc4945c461f2650bc2ef9491f2430
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-15 14:59:36 +0300

    fix `try` test spec

--- haskell/glue/test/Glue/Lib/Builtin/TrySpec.hs
+++ haskell/glue/test/Glue/Lib/Builtin/TrySpec.hs
@@ -23,15 +23,15 @@ runCode input = case parseGlue input of
 spec :: Spec
 spec = describe "Glue.Lib.Builtin.Try (Test try special form)" do
     it "catches exception and calls handler with payload" do
-        let code = "(try (error test-error (:msg \"hello\")) (catch \"test-error\" (lambda (err) err.msg)))"
+        let code = "(try (error test-error (:msg \"hello\")) (catch test-error (lambda (err) err.msg)))"
         runCode code `shouldReturn` Right (Just (String "hello"))
 
     it "returns normal value when no exception" do
-        let code = "(try 42 (catch \"any-error\" (lambda (err) \"caught\")))"
+        let code = "(try 42 (catch any-error (lambda (err) \"caught\")))"
         runCode code `shouldReturn` Right (Just (Integer 42))
 
     it "re-throws unmatched exception" do
-        let code = "(try (error test-error (:msg \"hello\")) (catch \"other-error\" (lambda (err) err.msg)))"
+        let code = "(try (error test-error (:msg \"hello\")) (catch other-error (lambda (err) err.msg)))"
         result <- runCode code
         case result of
             Left _ -> pure () -- Expected error
@@ -46,9 +46,9 @@ spec = describe "Glue.Lib.Builtin.Try (Test try special form)" do
         runCode code `shouldReturn` Right (Just (Integer 124))
 
     it "multiple catch clauses work" do
-        let code = "(try (error second-error (:msg \"second\")) (catch \"first-error\" (lambda (err) \"first\")) (catch \"second-error\" (lambda (err) err.msg)))"
+        let code = "(try (error second-error (:msg \"second\")) (catch first-error (lambda (err) \"first\")) (catch second-error (lambda (err) err.msg)))"
         runCode code `shouldReturn` Right (Just (String "second"))
 
     it "first matching catch is used" do
-        let code = "(try (error test-error (:msg \"caught\")) (catch \"test-error\" (lambda (err) err.msg)) (catch \"test-error\" (lambda (err) \"second\")))"
+        let code = "(try (error test-error (:msg \"caught\")) (catch test-error (lambda (err) err.msg)) (catch test-error (lambda (err) \"second\")))"
         runCode code `shouldReturn` Right (Just (String "caught"))
