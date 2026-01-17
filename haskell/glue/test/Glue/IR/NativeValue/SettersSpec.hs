2511c60921bc3c2376c3980fb1b188c1dffd283b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-17 14:44:07 +0300

    Implement comprehensive FFI setter tests with type checking and error handling

--- haskell/glue/test/Glue/IR/NativeValue/SettersSpec.hs
+++ haskell/glue/test/Glue/IR/NativeValue/SettersSpec.hs
@@ -2,33 +2,38 @@ module Glue.IR.NativeValue.SettersSpec (spec) where
 
 import Data.Map.Strict qualified as Map
 import Glue.Env qualified as E
-import Glue.Eval (Eval, runEvalSimple)
+import Glue.Eval (Eval, runEvalSimple, throwError)
+import Glue.Eval.Exception (wrongArgumentType)
 import Glue.IR (IR (..), hostValueWithProps)
 import Glue.Lib.Builtin.Set qualified as Set
 import Test.Hspec
 
 spec :: Spec
 spec = describe "HostValue property setters" do
-    describe "Property setting with setter functions" do
-        it "sets property using setter function" do
-            -- Create a setter function that just returns Void (simulating a side effect)
-            let labelSetter = NativeFunc (\[String _newLabel] -> pure Void)
-                setters = Map.fromList [("label", labelSetter)]
+    describe "Property setting with type checking" do
+        it "sets string property with correct type" do
+            -- Create a setter that validates string type
+            let nameSetter = NativeFunc $ \[newVal] -> case newVal of
+                    String _ -> pure Void -- Accept any string
+                    _ -> throwError $ wrongArgumentType ["string"]
+                setters = Map.fromList [("name", nameSetter)]
                 hostVal = hostValueWithProps () Map.empty setters
                 hostIr = NativeValue hostVal
 
             -- Set up environment and call set function
             let env = E.defineVar "obj" hostIr E.emptyEnv
-                setArgs = [Symbol "obj.label", String "updated"]
+                setArgs = [Symbol "obj.name", String "Alice"]
 
             result <- runEvalSimple (Set.set setArgs) env
             case result of
-                Right (Void, _) -> pure () -- Success - setter was called and returned Void
+                Right (Void, _) -> pure () -- Success
                 Left err -> expectationFailure $ "Property setting should succeed: " ++ show err
 
-        it "sets numeric property using setter function" do
-            -- Create a setter that accepts an integer
-            let sizeSetter = NativeFunc (\[Integer _newSize] -> pure Void)
+        it "sets numeric property with correct type" do
+            -- Create a setter that validates integer type
+            let sizeSetter = NativeFunc $ \[newVal] -> case newVal of
+                    Integer _ -> pure Void -- Accept any integer
+                    _ -> throwError $ wrongArgumentType ["integer"]
                 setters = Map.fromList [("size", sizeSetter)]
                 hostVal = hostValueWithProps () Map.empty setters
                 hostIr = NativeValue hostVal
@@ -42,31 +47,72 @@ spec = describe "HostValue property setters" do
                 Right (Void, _) -> pure () -- Success
                 Left err -> expectationFailure $ "Property setting should succeed: " ++ show err
 
-    describe "Multiple setters on same object" do
-        it "sets different properties on the same host value" do
-            let labelSetter = NativeFunc (\[String _newLabel] -> pure Void)
-                enabledSetter = NativeFunc (\[Bool _newEnabled] -> pure Void)
-                setters = Map.fromList [("label", labelSetter), ("enabled", enabledSetter)]
+    describe "Type checking and error handling" do
+        it "throws error when setting string property to wrong type" do
+            -- Create a setter that only accepts strings
+            let nameSetter = NativeFunc $ \[newVal] -> case newVal of
+                    String _ -> pure Void
+                    _ -> throwError $ wrongArgumentType ["string"]
+                setters = Map.fromList [("name", nameSetter)]
+                hostVal = hostValueWithProps () Map.empty setters
+                hostIr = NativeValue hostVal
+
+            -- Try to set name to an integer (wrong type)
+            let env = E.defineVar "obj" hostIr E.emptyEnv
+                setArgs = [Symbol "obj.name", Integer 123]
+
+            result <- runEvalSimple (Set.set setArgs) env
+            case result of
+                Left _ -> pure () -- Should fail with type error
+                Right (val, _) -> expectationFailure $ "Setting string property to integer should fail, but got: " ++ show val
+
+        it "throws error when setting numeric property to wrong type" do
+            -- Create a setter that only accepts integers
+            let ageSetter = NativeFunc $ \[newVal] -> case newVal of
+                    Integer _ -> pure Void
+                    _ -> throwError $ wrongArgumentType ["integer"]
+                setters = Map.fromList [("age", ageSetter)]
+                hostVal = hostValueWithProps () Map.empty setters
+                hostIr = NativeValue hostVal
+
+            -- Try to set age to a string (wrong type)
+            let env = E.defineVar "obj" hostIr E.emptyEnv
+                setArgs = [Symbol "obj.age", String "thirty"]
+
+            result <- runEvalSimple (Set.set setArgs) env
+            case result of
+                Left _ -> pure () -- Should fail with type error
+                Right (val, _) -> expectationFailure $ "Setting numeric property to string should fail, but got: " ++ show val
+
+    describe "Multiple properties with different types" do
+        it "sets different typed properties on the same object" do
+            let nameSetter = NativeFunc $ \[newVal] -> case newVal of
+                    String _ -> pure Void
+                    _ -> throwError $ wrongArgumentType ["string"]
+                ageSetter = NativeFunc $ \[newVal] -> case newVal of
+                    Integer _ -> pure Void
+                    _ -> throwError $ wrongArgumentType ["integer"]
+                setters = Map.fromList [("name", nameSetter), ("age", ageSetter)]
                 hostVal = hostValueWithProps () Map.empty setters
                 hostIr = NativeValue hostVal
 
             let env = E.defineVar "obj" hostIr E.emptyEnv
 
-            -- Set label
-            let setLabelArgs = [Symbol "obj.label", String "changed"]
-            result1 <- runEvalSimple (Set.set setLabelArgs) env
+            -- Set name (string)
+            let setNameArgs = [Symbol "obj.name", String "Bob"]
+            result1 <- runEvalSimple (Set.set setNameArgs) env
             case result1 of
                 Right (Void, _) -> pure ()
-                Left err -> expectationFailure $ "Label setting should succeed: " ++ show err
+                Left err -> expectationFailure $ "Name setting should succeed: " ++ show err
 
-            -- Set enabled
-            let setEnabledArgs = [Symbol "obj.enabled", Bool True]
-            result2 <- runEvalSimple (Set.set setEnabledArgs) env
+            -- Set age (integer)
+            let setAgeArgs = [Symbol "obj.age", Integer 25]
+            result2 <- runEvalSimple (Set.set setAgeArgs) env
             case result2 of
                 Right (Void, _) -> pure ()
-                Left err -> expectationFailure $ "Enabled setting should succeed: " ++ show err
+                Left err -> expectationFailure $ "Age setting should succeed: " ++ show err
 
-    describe "Error handling" do
+    describe "Error handling for missing properties" do
         it "fails when setting non-existent property" do
             let hostVal = hostValueWithProps () Map.empty Map.empty
                 hostIr = NativeValue hostVal
