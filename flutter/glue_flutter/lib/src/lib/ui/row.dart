1d2e4c8b467504ca567a34d73285fd924ba86dcf
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-21 23:00:54 +0300

    Extract row widget creation into separate function
    Separate pattern matching logic from widget creation logic.

--- flutter/glue_flutter/lib/src/lib/ui/row.dart
+++ flutter/glue_flutter/lib/src/lib/ui/row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:glue/src/eval.dart';
 import 'package:glue/src/ir.dart';
 import 'package:glue/src/eval/exception.dart';
@@ -9,17 +10,19 @@ import 'package:glue_flutter/src/utils/widget_properties.dart';
 final Ir row = IrNativeFunc(rowImpl);
 
 /// Row implementation - takes properties object
-Eval<Ir> rowImpl(Ir props) => switch (props) {
-  IrObject() => () {
-    // Extract properties using lazy wrapper
-    final properties = Properties(props.properties.unlock);
-
-    final rowWidget = Row(
-      children: properties.children,
-      mainAxisAlignment: properties.mainAlign,
-      crossAxisAlignment: properties.crossAlign,
-    );
-    return Eval.pure(IrNativeValue(HostValue(rowWidget)));
-  }(),
+Eval<Ir> rowImpl(Ir arg) => switch (arg) {
+  IrObject(:final properties) => _createRow(properties),
   _ => throwError(wrongArgumentType(['object'])),
 };
+
+/// Create Row widget from properties
+Eval<Ir> _createRow(IMap<String, Ir> properties) {
+  // Extract properties using lazy wrapper
+  final props = Properties(properties.unlock);
+  final rowWidget = Row(
+    children: props.children,
+    mainAxisAlignment: props.mainAlign,
+    crossAxisAlignment: props.crossAlign,
+  );
+  return Eval.pure(IrNativeValue(HostValue(rowWidget)));
+}
