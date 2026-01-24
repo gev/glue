5aa4a8d4641e153f63746db9da3c57f8358ea644
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-24 18:36:01 +0300

    Fix module import to use public API
    - Change import from glue/src/module.dart to glue/module.dart
    - Use public API exports instead of implementation imports
    - Follow Dart package import conventions
    - env.dart now uses proper public API imports

--- flutter/glue_demo/lib/services/env.dart
+++ flutter/glue_demo/lib/services/env.dart
@@ -1,6 +1,6 @@
 import 'package:glue/lib/bool.dart';
 import 'package:glue/lib/builtin.dart';
-import 'package:glue/src/module.dart';
+import 'package:glue/module.dart';
 import 'package:glue_flutter/glue_flutter.dart';
 import 'reactive_module.dart';
 
