a7d59f920bc93788a374a450f030eaa07b754ee5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-24 19:31:00 +0300

    Format code.dart with proper indentation
    - Add proper indentation to Glue code in defaultCode string
    - Improve code readability and formatting
    - Follow consistent code style guidelines

--- flutter/glue_demo/lib/code.dart
+++ flutter/glue_demo/lib/code.dart
@@ -1,16 +1,30 @@
 String defaultCode = '''
 ;; Welcome to Glue Demo!
 ;; Edit this code and see the UI update in real-time
-
+;; Welcome to Glue Demo!
+;; Edit this code and see the UI update in real-time
 (
-    (def my-counter (reactive-counter 0))
+   (def my-counter (reactive-counter 0))
 
-    (reactive-widget my-counter
-        (text
-            :content my-counter.value
-            :color colors.blue
-            :font-size 48
-        )
-    )
+   (text
+      :content "Hello Glue!"
+      :color colors.grey
+      :size 32)
+   
+   (reactive-widget my-counter
+      (text
+         :content my-counter.value
+         :color colors.blue
+         :size 128))
+   
+   (row
+      :main-axis-align main-axis-alignment.center
+      :children (
+         (button
+            :label "Increment"
+            :on-tap (\\ () (counter.increment 1)))
+         (button
+            :label "Decrement"
+            :on-tap (\\ () (counter.decrement 1)))))
 )
 ''';
