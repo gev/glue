e9d4964ebe2a60e74df579279380eb6747e40892
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-24 20:12:59 +0300

    commit

--- flutter/glue_demo/lib/services/reactive_counter_function.dart
+++ flutter/glue_demo/lib/services/reactive_counter_function.dart
@@ -1,7 +1,14 @@
 import 'package:glue/ir.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/either.dart';
+import 'package:glue/src/eval/exception.dart';
+import 'package:glue/src/eval/error.dart';
 import 'reactive_counter.dart';
+
+// Helper function for throwing runtime errors
+Eval<Ir> throwError(RuntimeException exception) {
+  return Eval((runtime) => Left(EvalError([], exception)));
+}
 
 /// Creates a reactive counter with HostValue getters and setters
 /// Returns IrNativeValue(HostValue(ReactiveCounter))
@@ -20,38 +27,17 @@ final reactiveCounter = IrNativeFunc((Ir initialValue) {
           'value': Eval(
             (runtime) => Right((IrString(counter.value.toString()), runtime)),
           ),
-          'increment': Eval(
-            (runtime) => Right((
-              IrNativeFunc((Ir amount) {
-                final amt = switch (amount) {
-                  IrInteger(value: final value) => value,
-                  _ => 1,
-                };
-                counter.increment(amt);
-                return Eval.pure(IrVoid());
-              }),
-              runtime,
-            )),
-          ),
-          'decrement': Eval(
-            (runtime) => Right((
-              IrNativeFunc((Ir amount) {
-                final amt = switch (amount) {
-                  IrInteger(value: final value) => value,
-                  _ => 1,
-                };
-                counter.decrement(amt);
-                return Eval.pure(IrVoid());
-              }),
-              runtime,
-            )),
-          ),
         },
         setters: {
-          'value': (Ir newValue) => Eval((runtime) {
-            counter.value = (newValue as IrInteger).value;
-            return Right((IrVoid(), runtime));
-          }),
+          'value': (Ir value) => switch (value) {
+            IrInteger(value: final newValue) => Eval<Ir>((runtime) {
+              counter.value = newValue;
+              return Right((IrVoid(), runtime));
+            }),
+            _ => throwError(
+              RuntimeException('wrong-argument-type', IrString('integer')),
+            ),
+          },
         },
       ),
     ),
