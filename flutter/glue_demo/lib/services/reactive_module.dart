d0519a62d72e4553a416eea817f162ea57047c0c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-25 17:30:43 +0300

    Add inc and dec functions to reactive module
    - Export inc and dec functions in reactive module
    - Functions now available in Glue code as (reactive.inc) and (reactive.dec)
    - Complete integration of counter manipulation functions

--- flutter/glue_demo/lib/services/reactive_module.dart
+++ flutter/glue_demo/lib/services/reactive_module.dart
@@ -5,5 +5,7 @@ import 'package:glue_demo/services/reactive_widget_function.dart';
 /// Reactive module providing reactive state management and UI components
 final reactiveModule = nativeModule('reactive', [
   ('reactive-counter', reactiveCounter),
+  ('inc', inc),
+  ('dec', dec),
   ('reactive-widget', reactiveWidget),
 ]);
