3463151d44aaf9115e0ea07d97ce327a91647021
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-30 00:40:21 +0300

    Fix cupertino_date_picker WidgetProperties API usage
    - Convert all direct property getters to proper getValue() methods
    - Use kebab-case naming convention for all properties
    - Maintain runtime wrapper for potential callback support

--- flutter/glue_flutter/lib/src/lib/ui/cupertino/widgets/cupertino_date_picker.dart
+++ flutter/glue_flutter/lib/src/lib/ui/cupertino/widgets/cupertino_date_picker.dart
@@ -19,27 +19,33 @@ Eval<Ir> cupertinoDatePickerImpl(Ir props) => switch (props) {
 /// Create CupertinoDatePicker widget from properties object
 Eval<Ir> _createCupertinoDatePicker(WidgetProperties properties) {
   return getRuntime().map((runtime) {
-    final callback = (DateTime value) =>
-        properties.cupertinoDatePickerOnDateTimeChanged(value);
-
     final initialDateTime =
-        properties.cupertinoDatePickerInitialDateTime ?? DateTime.now();
+        properties.getValue('cupertino-date-picker-initial-date-time') ??
+        DateTime.now();
 
     final pickerWidget = CupertinoDatePicker(
       key: GlobalKey(),
-      mode: properties.cupertinoDatePickerMode,
-      onDateTimeChanged: callback,
+      mode: properties.getValue('cupertino-date-picker-mode'),
+      onDateTimeChanged: properties.getValue(
+        'cupertino-date-picker-on-date-time-changed',
+      ),
       initialDateTime: initialDateTime,
-      minimumDate: properties.cupertinoDatePickerMinimumDate,
-      maximumDate: properties.cupertinoDatePickerMaximumDate,
-      minimumYear: properties.cupertinoDatePickerMinimumYear,
-      maximumYear: properties.cupertinoDatePickerMaximumYear,
-      minuteInterval: properties.cupertinoDatePickerMinuteInterval,
-      use24hFormat: properties.cupertinoDatePickerUse24hFormat,
-      dateOrder: properties.cupertinoDatePickerDateOrder,
-      backgroundColor: properties.cupertinoDatePickerBackgroundColor,
-      showDayOfWeek: properties.cupertinoDatePickerShowDayOfWeek,
-      itemExtent: properties.cupertinoDatePickerItemExtent,
+      minimumDate: properties.getValue('cupertino-date-picker-minimum-date'),
+      maximumDate: properties.getValue('cupertino-date-picker-maximum-date'),
+      minimumYear: properties.getValue('cupertino-date-picker-minimum-year'),
+      maximumYear: properties.getValue('cupertino-date-picker-maximum-year'),
+      minuteInterval: properties.getValue(
+        'cupertino-date-picker-minute-interval',
+      ),
+      use24hFormat: properties.getValue('cupertino-date-picker-use-24h-format'),
+      dateOrder: properties.getValue('cupertino-date-picker-date-order'),
+      backgroundColor: properties.getValue(
+        'cupertino-date-picker-background-color',
+      ),
+      showDayOfWeek: properties.getValue(
+        'cupertino-date-picker-show-day-of-week',
+      ),
+      itemExtent: properties.getValue('cupertino-date-picker-item-extent'),
     );
     return IrNativeValue(Value(pickerWidget));
   });
