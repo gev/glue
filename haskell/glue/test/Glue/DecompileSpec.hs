e919ea7d814e3b896b4702574bb36276c18fde63
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-02-23 23:08:15 +0300

    Remove AST arbitrary, keep only IR arbitrary

--- haskell/glue/test/Glue/DecompileSpec.hs
+++ haskell/glue/test/Glue/DecompileSpec.hs
@@ -4,8 +4,6 @@ module Glue.DecompileSpec (spec) where
 
 import Data.Functor.Identity (Identity)
 import Data.Map.Strict qualified as Map
-import Glue.AST (AST)
-import Glue.AST qualified as AST
 import Glue.Compile (compile)
 import Glue.Decompile (decompile)
 import Glue.IR (IR (..))
@@ -13,28 +11,6 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import Test.QuickCheck.Instances ()
-
-instance Arbitrary AST where
-    arbitrary = sized genGlue
-      where
-        genGlue n
-            | n <= 0 =
-                oneof
-                    [ AST.Symbol <$> arbitrary
-                    , AST.Integer <$> arbitrary
-                    , AST.Float <$> arbitrary
-                    , AST.String <$> arbitrary
-                    ]
-        genGlue n =
-            oneof
-                [ AST.Symbol <$> arbitrary
-                , AST.Integer <$> arbitrary
-                , AST.Float <$> arbitrary
-                , AST.String <$> arbitrary
-                , -- Generate nested structures
-                  AST.List <$> resize (n `div` 2) arbitrary
-                , AST.Object <$> resize (n `div` 2) arbitrary
-                ]
 
 -- Arbitrary for IR (only serializable types)
 instance Arbitrary (IR m) where
