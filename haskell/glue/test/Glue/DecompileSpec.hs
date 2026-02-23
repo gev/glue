5affbdf0a4a5d7a51e8cc185a5e2dfdaa64f16ce
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-02-23 23:06:32 +0300

    Add Arbitrary instance for IR and test directly on IR

--- haskell/glue/test/Glue/DecompileSpec.hs
+++ haskell/glue/test/Glue/DecompileSpec.hs
@@ -3,11 +3,12 @@
 module Glue.DecompileSpec (spec) where
 
 import Data.Functor.Identity (Identity)
+import Data.Map.Strict qualified as Map
 import Glue.AST (AST)
 import Glue.AST qualified as AST
 import Glue.Compile (compile)
 import Glue.Decompile (decompile)
-import Glue.IR (IR)
+import Glue.IR (IR (..))
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
@@ -35,13 +36,35 @@ instance Arbitrary AST where
                 , AST.Object <$> resize (n `div` 2) arbitrary
                 ]
 
+-- Arbitrary for IR (only serializable types)
+instance Arbitrary (IR m) where
+    arbitrary = sized genIR
+      where
+        genIR n
+            | n <= 0 =
+                oneof
+                    [ Integer <$> arbitrary
+                    , Float <$> arbitrary
+                    , String <$> arbitrary
+                    , Symbol <$> arbitrary
+                    ]
+        genIR n =
+            oneof
+                [ Integer <$> arbitrary
+                , Float <$> arbitrary
+                , String <$> arbitrary
+                , Symbol <$> arbitrary
+                , -- Generate nested structures
+                  List <$> resize (n `div` 2) (listOf1 (genIR (n `div` 2)))
+                , Object <$> resize (n `div` 2) (Map.fromList <$> listOf1 ((,) <$> arbitrary <*> resize (n `div` 2) arbitrary))
+                ]
+
 spec :: Spec
 spec = describe "IR -> AST transformation (decompile)" $ do
     describe "Roundtrip: IR -> AST -> IR" $ do
-        prop "decompile then compile returns equivalent IR" $ \(ast :: AST) -> do
-            let ir1 = compile ast :: IR Identity
-            case decompile ir1 of
+        prop "decompile then compile returns equivalent IR" $ \(ir :: IR Identity) -> do
+            case decompile ir of
                 Left _ -> expectationFailure "Expected Right"
-                Right ast2 -> do
-                    let ir2 = compile ast2 :: IR Identity
-                    show ir1 `shouldBe` show ir2
+                Right ast -> do
+                    let ir2 = compile ast :: IR Identity
+                    show ir `shouldBe` show ir2
