69f57610375c9ff2d4a66bb4ebaa291c7e9596e1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-03 01:26:31 +0300

    Fix

--- dart/glue/lib/src/lib/builtin/lambda.dart
+++ dart/glue/lib/src/lib/builtin/lambda.dart
@@ -47,5 +47,8 @@ Either<RuntimeException, List<String>> extractSymbols(List<Ir> irs) {
 /// Create closure with parameters and body
 /// Mirrors Haskell makeClosure exactly - stores all params directly
 Eval<Ir> makeClosure(List<String> params, Ir body) {
-  return getEnv().map((env) => IrClosure(params, body, env));
+  // return getEnv().map((env) => IrClosure(params, body, env));
+  return Eval(
+    (runtime) => Right((IrClosure(params, body, runtime.env), runtime)),
+  );
 }
