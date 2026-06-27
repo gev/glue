a6759c55c29c1621a76bca2acee60f5103f0c0c7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-28 01:40:37 +0300

    Use kebab-case for clip style property names

--- flutter/glue_flutter/lib/src/lib/ui/core/styles/clip.dart
+++ flutter/glue_flutter/lib/src/lib/ui/core/styles/clip.dart
@@ -5,7 +5,9 @@ import 'package:glue/ir.dart';
 /// Each property is a NativeValue wrapping the Flutter Clip enum value
 final clip = IrObject({
   'none': IrNativeValue(Value(Clip.none)),
-  'hardEdge': IrNativeValue(Value(Clip.hardEdge)),
-  'antiAlias': IrNativeValue(Value(Clip.antiAlias)),
-  'antiAliasWithSaveLayer': IrNativeValue(Value(Clip.antiAliasWithSaveLayer)),
+  'hard-edge': IrNativeValue(Value(Clip.hardEdge)),
+  'anti-alias': IrNativeValue(Value(Clip.antiAlias)),
+  'anti-alias-with-save-layer': IrNativeValue(
+    Value(Clip.antiAliasWithSaveLayer),
+  ),
 });
