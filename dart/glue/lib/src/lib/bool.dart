b9d307dcc4eace0f37e4f66c88c7428b76df6c82
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-29 16:57:33 +0300

    Add fallback and maybe boolean operations to handle optional values

--- dart/glue/lib/src/lib/bool.dart
+++ dart/glue/lib/src/lib/bool.dart
@@ -1,6 +1,7 @@
 import 'package:glue/src/ir.dart';
 import 'package:glue/src/lib/bool/and.dart';
 import 'package:glue/src/lib/bool/eq.dart';
+import 'package:glue/src/lib/bool/fallback.dart';
 import 'package:glue/src/lib/bool/ge.dart';
 import 'package:glue/src/lib/bool/gt.dart';
 import 'package:glue/src/lib/bool/if.dart';
@@ -8,6 +9,7 @@ import 'package:glue/src/lib/bool/is_empty.dart';
 import 'package:glue/src/lib/bool/is_exist.dart';
 import 'package:glue/src/lib/bool/le.dart';
 import 'package:glue/src/lib/bool/lt.dart';
+import 'package:glue/src/lib/bool/maybe.dart';
 import 'package:glue/src/lib/bool/ne.dart';
 import 'package:glue/src/lib/bool/not.dart';
 import 'package:glue/src/lib/bool/or.dart';
@@ -42,6 +44,12 @@ final ModuleInfo boolModule = nativeModule('ffi.bool', [
 
   ('is-empty', isEmpty_),
   ('is-exist', isExist_),
+
+  ('fallback', fallback),
+  ('??', fallback),
+
+  ('maybe', maybe),
+  ('?>', maybe),
 
   ('if', if_),
   ('?', if_),
