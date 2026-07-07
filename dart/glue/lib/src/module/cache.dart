593a92952d58ab25cdafd89668b3882529eb695c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-07 13:23:44 +0300

    Update cache.dart

--- dart/glue/lib/src/module/cache.dart
+++ dart/glue/lib/src/module/cache.dart
@@ -19,10 +19,6 @@ void storeImportedModule(ImportedModuleCache cache, ImportedModule imported) {
       final ref = imported.frame[entry.key];
       entry.value.value = ref?.value ?? IrVoid();
     }
-    for (final entry in cached.exportedValues.entries) {
-      final ref = imported.exportedValues[entry.key];
-      entry.value.value = ref?.value ?? IrVoid();
-    }
     for (final entry in imported.exportedValues.entries) {
       final ref = cached.exportedValues[entry.key];
       if (ref == null) {
