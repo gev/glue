5767d23a5850331f6f982cb2c3e0c74461cd67ab
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-19 01:13:55 +0300

    Reorder alignment properties by vertical and horizontal positions

--- flutter/glue_flutter/lib/src/lib/ui/core/styles/alignment.dart
+++ flutter/glue_flutter/lib/src/lib/ui/core/styles/alignment.dart
@@ -4,13 +4,13 @@ import 'package:glue/ir.dart';
 /// Axis enum object - represents all Axis values as Glue object properties
 /// Each property is a NativeValue wrapping the Flutter Axis enum value
 final alignment = IrObject({
-  'bottom-center': IrNativeValue(Value(Alignment.bottomCenter)),
-  'bottom-left': IrNativeValue(Value(Alignment.bottomLeft)),
-  'bottom-right': IrNativeValue(Value(Alignment.bottomRight)),
-  'center': IrNativeValue(Value(Alignment.center)),
-  'center-left': IrNativeValue(Value(Alignment.centerLeft)),
-  'center-right': IrNativeValue(Value(Alignment.centerRight)),
-  'top-center': IrNativeValue(Value(Alignment.topCenter)),
   'top-left': IrNativeValue(Value(Alignment.topLeft)),
+  'top-center': IrNativeValue(Value(Alignment.topCenter)),
   'top-right': IrNativeValue(Value(Alignment.topRight)),
+  'center-left': IrNativeValue(Value(Alignment.centerLeft)),
+  'center': IrNativeValue(Value(Alignment.center)),
+  'center-right': IrNativeValue(Value(Alignment.centerRight)),
+  'bottom-left': IrNativeValue(Value(Alignment.bottomLeft)),
+  'bottom-center': IrNativeValue(Value(Alignment.bottomCenter)),
+  'bottom-right': IrNativeValue(Value(Alignment.bottomRight)),
 });
