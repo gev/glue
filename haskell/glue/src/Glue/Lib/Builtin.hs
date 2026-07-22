fd460a51c82bf76d2ccce6d39e28d0ea1a9ffd60
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-22 23:20:34 +0300

    Add list and void builtins with aliases

--- haskell/glue/src/Glue/Lib/Builtin.hs
+++ haskell/glue/src/Glue/Lib/Builtin.hs
@@ -5,8 +5,10 @@ import Glue.Lib.Builtin.Def (def)
 import Glue.Lib.Builtin.Error (errorFunc)
 import Glue.Lib.Builtin.Lambda (lambda)
 import Glue.Lib.Builtin.Let (let')
+import Glue.Lib.Builtin.List (list)
 import Glue.Lib.Builtin.Quote (quote)
 import Glue.Lib.Builtin.Try (tryFunc)
+import Glue.Lib.Builtin.Void (void)
 import Glue.Module (ModuleInfo, nativeModule)
 
 builtin :: ModuleInfo Eval
@@ -20,4 +22,8 @@ builtin =
         , ("error", errorFunc)
         , ("try", tryFunc)
         , ("quote", quote)
+        , ("list", list)
+        , ("#", list)
+        , ("void", void)
+        , ("_", void)
         ]
