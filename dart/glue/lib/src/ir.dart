b089d1a2150a695d95479aa52d864832b30d6397
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-28 16:46:59 +0300

    Update extractValue to use type check with when Use when clause to ensure value is of type T or it's subtype

--- dart/glue/lib/src/ir.dart
+++ dart/glue/lib/src/ir.dart
@@ -33,7 +33,7 @@ Value hostValueWithProps(dynamic value, Map<String, Eval<Ir>> getters) =>
 /// Extract a host value with type safety
 /// Mirrors Haskell extractValue exactly
 T? extractValue<T>(Value hostValue) => switch (hostValue) {
-  Value(value: T v) => v,
+  Value(:final value) when value is T => value,
   _ => null,
 };
 
