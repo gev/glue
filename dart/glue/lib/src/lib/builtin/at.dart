eb8240a6abd4aa97fb8ee213e9100d5102bd4939
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-29 03:13:56 +0300

    Add native value property lookup for at accessor

--- dart/glue/lib/src/lib/builtin/at.dart
+++ dart/glue/lib/src/lib/builtin/at.dart
@@ -29,6 +29,15 @@ final Ir at = IrNativeFunc((Ir target) {
         case IrObject(:final properties) when accessor is IrDottedSymbol:
           return _nestedLookup(accessor.parts, properties.unlock);
 
+        case IrNativeValue(:final value) when accessor is IrString:
+          return value.getters[accessor.value] ?? Eval.pure(IrVoid());
+
+        case IrNativeValue(:final value) when accessor is IrSymbol:
+          return value.getters[accessor.value] ?? Eval.pure(IrVoid());
+
+        case IrNativeValue(:final value) when accessor is IrDottedSymbol:
+          return _nestedNativeLookup(accessor.parts, value.getters);
+
         default:
           return throwError(wrongArgumentType(['target', 'accessor']));
       }
@@ -45,3 +54,21 @@ Eval<Ir> _nestedLookup(List<String> fields, Map<String, Ir> obj) =>
         _ => Eval.pure(IrVoid()),
       },
     };
+
+Eval<Ir> _nestedNativeLookup(
+  List<String> fields,
+  Map<String, Eval<Ir>> getters,
+) => switch (fields) {
+  [] => Eval.pure(IrVoid()),
+  [final key] => getters[key] ?? Eval.pure(IrVoid()),
+  [final key, ...final rest] => switch (getters[key]) {
+    null => Eval.pure(IrVoid()),
+    final evalAction => evalAction.bind(
+      (resolvedIR) => switch (resolvedIR) {
+        IrObject(:final properties) => _nestedLookup(rest, properties.unlock),
+        IrNativeValue(:final value) => _nestedNativeLookup(rest, value.getters),
+        _ => Eval.pure(IrVoid()),
+      },
+    ),
+  },
+};
