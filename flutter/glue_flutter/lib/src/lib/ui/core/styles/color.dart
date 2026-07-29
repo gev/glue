2fa16ef88e71d33d8e10a1a739411a5ab4c9c0f0
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-30 02:45:16 +0300

    Add Flutter Color IR representation
    Converts Flutter Color objects to IR native values with alpha, RGB,
    luminance getters and a `with` method for creating modified colors.

---
+++ flutter/glue_flutter/lib/src/lib/ui/core/styles/color.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/widgets.dart';
+import 'package:glue/error.dart';
+import 'package:glue/eval.dart';
+import 'package:glue/ir.dart';
+
+Ir makeColor(Color c) {
+  return IrNativeValue(
+    Value(
+      c,
+      getters: {
+        'alpha': _float(c.a),
+        'r': _float(c.r),
+        'g': _float(c.g),
+        'b': _float(c.b),
+        'luminance': _float(c.computeLuminance()),
+        'with': _with(c),
+      },
+    ),
+  );
+}
+
+Eval<Ir> _float(double v) {
+  return Eval.pure(IrFloat(v));
+}
+
+Eval<Ir> _with<T>(Color c) {
+  return Eval.pure(
+    IrNativeFunc((props) {
+      return switch (props) {
+        IrObject(:final properties) => Eval.pure(
+          makeColor(
+            c.withValues(
+              alpha: toDouble(properties['a']),
+              red: toDouble(properties['r']),
+              green: toDouble(properties['g']),
+              blue: toDouble(properties['b']),
+            ),
+          ),
+        ),
+        _ => throwError(
+          wrongArgumentType(['`Object` color properties required']),
+        ),
+      };
+    }),
+  );
+}
