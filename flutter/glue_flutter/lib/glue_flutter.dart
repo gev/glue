6db08209a3c6dca231b38587b37c661070d878dc
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-30 02:45:34 +0300

    Add color.dart export to glue_flutter library

--- flutter/glue_flutter/lib/glue_flutter.dart
+++ flutter/glue_flutter/lib/glue_flutter.dart
@@ -4,5 +4,6 @@ library;
 export 'src/lib/core.dart';
 export 'src/lib/cupertino.dart';
 export 'src/lib/material.dart';
+export 'src/lib/ui/core/styles//color.dart';
 export 'src/lib/ui/core/widgets/image.dart';
 export 'src/utils/widget_properties.dart';
