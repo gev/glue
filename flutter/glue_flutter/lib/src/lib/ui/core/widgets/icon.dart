aa0c64a00895b1806bcd2daba43305f5ab8c5192
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-31 21:50:30 +0300

    Add extra icon styling properties

--- flutter/glue_flutter/lib/src/lib/ui/core/widgets/icon.dart
+++ flutter/glue_flutter/lib/src/lib/ui/core/widgets/icon.dart
@@ -22,9 +22,16 @@ Eval<Ir> _createIcon(WidgetProperties properties) {
     properties.getValue<IconData>('icon'),
     key: properties.key,
     size: properties.getDouble('size'),
+    fill: properties.getDouble('fill'),
+    weight: properties.getDouble('weight'),
+    grade: properties.getDouble('grade'),
+    opticalSize: properties.getDouble('optical-size'),
     color: properties.getColor('color'),
+    shadows: properties.getValues<Shadow>('shadows'),
     semanticLabel: properties.getString('semantic-label'),
     textDirection: properties.getValue<TextDirection>('text-direction'),
+    blendMode: properties.getValue<BlendMode>('blend-mode'),
+    fontWeight: properties.getValue<FontWeight>('font-weight'),
   );
   return Eval.pure(IrNativeValue(Value(iconWidget)));
 }
