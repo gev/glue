3a9012a4b71e99686b5cf8e7834dec538bec0681
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-08-05 18:24:47 +0300

    Add io.dart library

---
+++ dart/glue/lib/lib/io.dart
@@ -0,0 +1,5 @@
+/// List operations and sequence functions for Glue language
+/// Provides functions for working with lists, sequences, and collections
+library;
+
+export 'package:glue/src/lib/io.dart';
