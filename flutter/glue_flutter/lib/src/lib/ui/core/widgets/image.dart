43e947014235bd65ad7f27d49f24c7fa0e2754a2
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-08-11 15:10:05 +0300

    Refactor image implementation to use ImageProvider pattern

--- flutter/glue_flutter/lib/src/lib/ui/core/widgets/image.dart
+++ flutter/glue_flutter/lib/src/lib/ui/core/widgets/image.dart
@@ -8,22 +8,61 @@ import 'package:glue_flutter/src/utils/widget_properties.dart';
 
 /// Image widget function
 /// Creates Flutter Image from Glue (image props) expressions
-final Ir imageAsset = IrNativeFunc(imageImpl(assetImage));
-final Ir imageFile = IrNativeFunc(imageImpl(fileImage));
-final Ir imageNetwork = IrNativeFunc(imageImpl(networkImage));
+final image = IrNativeFunc((Ir props) {
+  switch (props) {
+    case IrObject obj:
+      final properties = WidgetProperties(obj.properties.unlock);
+      final image = properties.getValue<ImageProvider>('image');
+      if (image == null) {
+        return throwError(
+          wrongArgumentType(['Image provider property `image` required']),
+        );
+      }
+      final imageWidget = Image(
+        key: properties.key,
+        image: image,
+        width: properties.width,
+        height: properties.height,
+        fit: properties.getValue<BoxFit>('fit'),
+        color: properties.getColor('color'),
+        colorBlendMode: properties.getValue<BlendMode>('color-blend-mode'),
+        alignment:
+            properties.getValue<AlignmentGeometry>('alignment') ??
+            Alignment.center,
+        repeat:
+            properties.getValue<ImageRepeat>('repeat') ?? ImageRepeat.noRepeat,
+        centerSlice: properties.getValue<Rect>('center-slice'),
+        matchTextDirection: properties.getBool('match-text-direction') ?? false,
+        gaplessPlayback: properties.getBool('gapless-playback') ?? false,
+        semanticLabel: properties.getString('semantic-label'),
+        excludeFromSemantics:
+            properties.getBool('exclude-from-semantics') ?? false,
+        filterQuality:
+            properties.getValue<FilterQuality>('filter-quality') ??
+            FilterQuality.medium,
+      );
+      return Eval.pure(IrNativeValue(Value(imageWidget)));
+    default:
+      return throwError(wrongArgumentType(['object']));
+  }
+});
 
-Eval<Ir> Function(Ir props) imageImpl(
+/// Image provider functions
+final Ir imageAsset = IrNativeFunc(imageProviderImpl(assetImage));
+final Ir imageFile = IrNativeFunc(imageProviderImpl(fileImage));
+final Ir imageNetwork = IrNativeFunc(imageProviderImpl(networkImage));
+
+Eval<Ir> Function(Ir props) imageProviderImpl(
   ImageProvider Function(String src) makeImage,
 ) => (Ir props) {
   switch (props) {
-    case IrObject object:
-      final properties = WidgetProperties(object.properties.unlock);
-      final src = properties.get('src');
+    case IrObject(:final properties):
+      final src = properties['src'];
       switch (src) {
         case IrString(:final value):
         case IrSymbol(:final value):
         case IrDottedSymbol(:final value):
-          return _createImage(makeImage(value), properties);
+          return Eval.pure(IrNativeValue(Value(makeImage(value))));
         default:
           return throwError(
             wrongArgumentType([
@@ -39,27 +78,3 @@ Eval<Ir> Function(Ir props) imageImpl(
 ImageProvider assetImage(String src) => AssetImage(src);
 ImageProvider fileImage(String src) => FileImage(File(src));
 ImageProvider networkImage(String src) => NetworkImage(src);
-
-Eval<Ir> _createImage(ImageProvider image, WidgetProperties properties) {
-  final imageWidget = Image(
-    key: properties.key,
-    image: image,
-    width: properties.width,
-    height: properties.height,
-    fit: properties.getValue<BoxFit>('fit'),
-    color: properties.getColor('color'),
-    colorBlendMode: properties.getValue<BlendMode>('color-blend-mode'),
-    alignment:
-        properties.getValue<AlignmentGeometry>('alignment') ?? Alignment.center,
-    repeat: properties.getValue<ImageRepeat>('repeat') ?? ImageRepeat.noRepeat,
-    centerSlice: properties.getValue<Rect>('center-slice'),
-    matchTextDirection: properties.getBool('match-text-direction') ?? false,
-    gaplessPlayback: properties.getBool('gapless-playback') ?? false,
-    semanticLabel: properties.getString('semantic-label'),
-    excludeFromSemantics: properties.getBool('exclude-from-semantics') ?? false,
-    filterQuality:
-        properties.getValue<FilterQuality>('filter-quality') ??
-        FilterQuality.medium,
-  );
-  return Eval.pure(IrNativeValue(Value(imageWidget)));
-}
