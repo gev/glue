ba3f4623762a33261262cd16f7b1d518220eb283
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-08-19 16:23:23 +0300

    Add BorderSide binder for Glue

---
+++ flutter/glue_flutter/lib/src/lib/ui/core/styles/border_side.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+import 'package:glue/eval.dart';
+import 'package:glue/ir.dart';
+import 'package:glue_flutter/src/utils/widget_properties.dart';
+
+/// BorderSide binder for Glue
+final Ir borderSide = IrNativeFunc(borderSideImpl);
+
+Eval<Ir> borderSideImpl(Ir props) => switch (props) {
+  IrObject(:final properties) => _createBorderSide(
+    WidgetProperties(properties.unlock),
+  ),
+  _ => Eval.pure(IrNativeValue(Value(const BorderSide()))),
+};
+
+Eval<Ir> _createBorderSide(WidgetProperties properties) {
+  return getRuntime().map((runtime) {
+    final side = BorderSide(
+      color: properties.getValue<Color>('color') ?? const Color(0xFF000000),
+      width: properties.getDouble('width') ?? 1.0,
+      style: properties.getValue<BorderStyle>('style') ?? BorderStyle.solid,
+      strokeAlign:
+          properties.getDouble('stroke-align') ?? BorderSide.strokeAlignInside,
+    );
+
+    return IrNativeValue(Value(side));
+  });
+}
