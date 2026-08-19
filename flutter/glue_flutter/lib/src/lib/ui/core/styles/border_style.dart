edf119a9cd4b840d9f40c6d20dbf81197bd680a5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-08-19 16:23:40 +0300

    Add border style definitions for Flutter

---
+++ flutter/glue_flutter/lib/src/lib/ui/core/styles/border_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:glue/ir.dart';
+
+/// BorderStyle object - represents available border styles as Glue object properties
+/// Each property is a NativeValue wrapping the Flutter BorderStyle constant
+final borderStyles = IrObject({
+  'solid': IrNativeValue(Value(BorderStyle.solid)),
+  'none': IrNativeValue(Value(BorderStyle.none)),
+});
