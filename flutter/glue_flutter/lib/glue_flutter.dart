f75ff4d5546ce642b68b8708c6702db9352a3c04
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-08-22 02:24:26 +0300

    Fix duplicate slash and add utility exports
    Fixed duplicate slash in color.dart export path.
    Added exports for value_extractors.dart and widget_properties.dart.

--- flutter/glue_flutter/lib/glue_flutter.dart
+++ flutter/glue_flutter/lib/glue_flutter.dart
@@ -4,6 +4,7 @@ library;
 export 'src/lib/core.dart';
 export 'src/lib/cupertino.dart';
 export 'src/lib/material.dart';
-export 'src/lib/ui/core/styles//color.dart';
+export 'src/lib/ui/core/styles/color.dart';
 export 'src/lib/ui/core/widgets/image.dart';
+export 'src/utils/value_extractors.dart';
 export 'src/utils/widget_properties.dart';
