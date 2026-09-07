f2683a793955d3d385ccc5f2df14308d513ff9a9
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-07 12:34:56 +0300

    Update segmented_button.dart

--- flutter/glue_flutter/lib/src/lib/ui/material/widgets/segmented_button.dart
+++ flutter/glue_flutter/lib/src/lib/ui/material/widgets/segmented_button.dart
@@ -21,8 +21,8 @@ Eval<Ir> segmentedButtonImpl(Ir props) => switch (props) {
 Eval<Ir> _createSegmentedButton(WidgetProperties properties) {
   return getRuntime().map((runtime) {
     print(properties);
-    return IrNativeValue(Value(SizedBox()));
     print(toList<Ir>(properties.get('selected')).toSet());
+    return IrNativeValue(Value(SizedBox()));
     print(properties.getValues<ButtonSegment<Ir>>('segments'));
     final segmentedButtonWidget = SegmentedButton<Ir>(
       key: properties.key,
