87dc95c0b87608043df1a0f994fcb76c624dee0f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-07 14:56:55 +0300

    Update button_segment.dart

--- flutter/glue_flutter/lib/src/lib/ui/material/widgets/button_segment.dart
+++ flutter/glue_flutter/lib/src/lib/ui/material/widgets/button_segment.dart
@@ -17,8 +17,8 @@ Eval<Ir> buttonSegmentImpl(Ir props) => switch (props) {
 
 /// Create ButtonSegment from properties
 Eval<Ir> _createButtonSegment(WidgetProperties properties) {
-  final segment = ButtonSegment(
-    value: properties.get('value') ?? IrVoid,
+  final segment = ButtonSegment<Ir>(
+    value: properties.get('value') ?? IrVoid(),
     label: properties.getValue<Widget>('label'),
     icon: properties.getValue<Widget>('icon'),
     tooltip: properties.getValue<String>('tooltip'),
