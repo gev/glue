e6ef507d801c2227a4639ccf72313d079ddba500
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-07 15:00:37 +0300

    Update button_segment.dart

--- flutter/glue_flutter/lib/src/lib/ui/material/widgets/button_segment.dart
+++ flutter/glue_flutter/lib/src/lib/ui/material/widgets/button_segment.dart
@@ -24,10 +24,5 @@ Eval<Ir> _createButtonSegment(WidgetProperties properties) {
     tooltip: properties.getValue<String>('tooltip'),
     enabled: properties.getBool('enabled') ?? true,
   );
-  final res = IrNativeValue(Value(segment));
-  print(segment.runtimeType);
-  print(res.runtimeType);
-  print(res.value.runtimeType);
-  print(res.value.value.runtimeType);
-  return Eval.pure(res);
+  return Eval.pure(IrNativeValue(Value(segment)));
 }
