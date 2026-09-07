0f367f3d7977aec77c104f8da74eb6b10e23a02f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-07 12:42:07 +0300

    Fix segmented button and extractor syntax
    Remove debug prints and SizedBox fallback from segmented button
    Fix pattern syntax in extractNativeValues

--- flutter/glue_flutter/lib/src/lib/ui/material/widgets/segmented_button.dart
+++ flutter/glue_flutter/lib/src/lib/ui/material/widgets/segmented_button.dart
@@ -20,10 +20,6 @@ Eval<Ir> segmentedButtonImpl(Ir props) => switch (props) {
 /// Create SegmentedButton widget from properties
 Eval<Ir> _createSegmentedButton(WidgetProperties properties) {
   return getRuntime().map((runtime) {
-    print(properties);
-    print(toList<Ir>(properties.get('selected')).toSet());
-    print(properties.getValues<ButtonSegment>('segments'));
-    return IrNativeValue(Value(SizedBox()));
     final segmentedButtonWidget = SegmentedButton<Ir>(
       key: properties.key,
       selected: toList<Ir>(properties.get('selected')).toSet(),
