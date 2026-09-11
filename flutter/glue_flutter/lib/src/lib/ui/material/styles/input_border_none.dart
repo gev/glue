15b058998aed065a0a46e0cbd04ee9e18da09a7c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-11 17:52:31 +0300

    Add InputBorder.none constant glue value

---
+++ flutter/glue_flutter/lib/src/lib/ui/material/styles/input_border_none.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:glue/ir.dart';
+
+/// InputBorder.none constant function
+/// Returns InputBorder.none for Flutter InputDecoration borders
+final Ir inputBorderNone = IrNativeValue(Value(InputBorder.none));
